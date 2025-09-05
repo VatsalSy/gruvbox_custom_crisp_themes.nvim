4c52e8850fcf31841a08542210b320794618a13c
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-09-05 18:06:41 +0100

    feat(highlights): high-contrast highlight groups and Treesitter links
    - Map comments/strings/keywords/functions/numbers/types/operators
    - UI accents: borders, selection, cursor, diagnostics, diff
    - Basic Treesitter/semantic token links

---
+++ lua/gruvbox_crisp/groups.lua
@@ -0,0 +1,142 @@
+local M = {}
+
+local function bool(b)
+  if b then return true else return nil end
+end
+
+function M.get(p, o)
+  local transparent = o.transparent
+
+  local groups = {
+    -- Core/editor
+    Normal       = { fg = p.fg0, bg = transparent and p.none or p.bg0 },
+    NormalNC     = { fg = p.fg0, bg = transparent and p.none or p.bg0 },
+    NormalFloat  = { fg = p.fg0, bg = transparent and p.none or p.bg1 },
+    FloatBorder  = { fg = p.border_focus, bg = transparent and p.none or p.bg1 },
+    SignColumn   = { fg = p.fg1, bg = transparent and p.none or p.bg0 },
+    ColorColumn  = { bg = p.bg1 },
+    CursorLine   = { bg = p.bg1 },
+    Cursor       = { fg = p.bg0, bg = p.cursor, bold = true },
+    CursorLineNr = { fg = p.line_nr_active, bold = true },
+    LineNr       = { fg = p.gray },
+    WinSeparator = { fg = p.border_focus, bg = transparent and p.none or p.bg0 },
+    VertSplit    = { link = "WinSeparator" },
+    StatusLine   = { fg = p.bg0, bg = p.border_focus, bold = true },
+    StatusLineNC = { fg = p.fg1, bg = p.bg2 },
+    TabLine      = { fg = p.fg1, bg = p.bg1 },
+    TabLineSel   = { fg = p.bg0, bg = p.border_focus, bold = true },
+    TabLineFill  = { fg = p.fg1, bg = p.bg1 },
+
+    -- Menus / popups
+    Pmenu        = { fg = p.fg0, bg = p.bg1 },
+    PmenuSel     = { fg = p.fg0, bg = p.hover_bg, bold = true },
+    PmenuSbar    = { bg = p.bg2 },
+    PmenuThumb   = { bg = p.bg3 },
+
+    -- Search / selection
+    Search       = { fg = p.bg0, bg = p.keyword, bold = true },
+    IncSearch    = { fg = p.bg0, bg = p.operator, bold = true },
+    CurSearch    = { link = "IncSearch" },
+    Visual       = { bg = p.selection },
+    MatchParen   = { fg = p.decorator, bold = true },
+
+    -- Syntax (vim)
+    Comment      = { fg = p.comment, italic = bool(o.italics.comments) },
+    String       = { fg = p.string, italic = bool(o.italics.strings) },
+    Character    = { link = "String" },
+    Number       = { fg = p.number },
+    Boolean      = { fg = p.number },
+    Float        = { link = "Number" },
+    Identifier   = { fg = p.variable, italic = bool(o.italics.variables) },
+    Function     = { fg = p.func, bold = bool(o.bold.functions), italic = bool(o.italics.functions) },
+    Statement    = { fg = p.keyword },
+    Conditional  = { fg = p.keyword, italic = bool(o.italics.keywords) },
+    Repeat       = { fg = p.keyword },
+    Label        = { fg = p.keyword },
+    Operator     = { fg = p.operator },
+    Keyword      = { fg = p.keyword, italic = bool(o.italics.keywords) },
+    Exception    = { fg = p.keyword },
+    PreProc      = { fg = p.decorator },
+    Include      = { fg = p.decorator },
+    Define       = { fg = p.decorator },
+    Macro        = { fg = p.decorator },
+    PreCondit    = { fg = p.decorator },
+    Type         = { fg = p.type },
+    StorageClass = { fg = p.type },
+    Structure    = { fg = p.type },
+    Typedef      = { fg = p.type },
+    Special      = { fg = p.link },
+    SpecialChar  = { fg = p.link },
+    Tag          = { fg = p.operator },
+    Delimiter    = { fg = p.fg0 },
+    SpecialComment = { fg = p.comment, italic = bool(o.italics.comments) },
+    Debug        = { fg = p.red },
+    Underlined   = { underline = true },
+    Bold         = { bold = true },
+    Italic       = { italic = true },
+
+    -- Messages
+    Error        = { fg = p.red, bold = true },
+    WarningMsg   = { fg = p.warn },
+    ErrorMsg     = { fg = p.red, bold = true },
+    HintMsg      = { fg = p.hint },
+    Todo         = { fg = p.operator, bold = true },
+
+    -- Diff / VCS
+    DiffAdd      = { fg = p.green, bg = p.diff_add_bg },
+    DiffChange   = { fg = p.warn,  bg = p.bg2 },
+    DiffDelete   = { fg = p.red,   bg = p.diff_delete_bg },
+    DiffText     = { fg = p.diff_text, bg = p.bg2, bold = true },
+    GitSignsAdd    = { fg = p.green },
+    GitSignsChange = { fg = p.warn },
+    GitSignsDelete = { fg = p.red },
+
+    -- Diagnostics
+    DiagnosticError = { fg = p.red },
+    DiagnosticWarn  = { fg = p.warn },
+    DiagnosticInfo  = { fg = p.info },
+    DiagnosticHint  = { fg = p.hint },
+    DiagnosticOk    = { fg = p.ok },
+    DiagnosticUnderlineError = { sp = p.red, undercurl = true },
+    DiagnosticUnderlineWarn  = { sp = p.warn, undercurl = true },
+    DiagnosticUnderlineInfo  = { sp = p.info, undercurl = true },
+    DiagnosticUnderlineHint  = { sp = p.hint, undercurl = true },
+
+    -- LSP/UX
+    LspReferenceText  = { bg = p.hover_bg },
+    LspReferenceRead  = { bg = p.hover_bg },
+    LspReferenceWrite = { bg = p.hover_bg },
+    LspCodeLens       = { fg = p.comment },
+    LspInlayHint      = { fg = p.comment, bg = p.bg0 },
+
+    -- Treesitter/Semantic tokens (common)
+    ["@comment"]        = { link = "Comment" },
+    ["@string"]         = { link = "String" },
+    ["@number"]         = { link = "Number" },
+    ["@boolean"]        = { link = "Boolean" },
+    ["@constant"]       = { fg = p.number },
+    ["@variable"]       = { fg = p.variable },
+    ["@variable.parameter"] = { fg = p.variable },
+    ["@field"]          = { fg = p.variable },
+    ["@function"]       = { link = "Function" },
+    ["@method"]         = { link = "Function" },
+    ["@keyword"]        = { link = "Keyword" },
+    ["@operator"]       = { link = "Operator" },
+    ["@type"]           = { link = "Type" },
+    ["@type.builtin"]   = { link = "Type" },
+    ["@namespace"]      = { fg = p.number }, -- bright purple
+    ["@typeParameter"]  = { link = "Type" },
+    ["@attribute"]      = { fg = p.decorator },
+
+    -- UI links
+    Underlined         = { fg = p.link, underline = true },
+  }
+
+  for k, v in pairs(o.overrides or {}) do
+    groups[k] = v
+  end
+
+  return groups
+end
+
+return M
